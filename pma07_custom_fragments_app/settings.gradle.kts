36db4c770be0a582f3e0989660efcdb3a27ae513
OndrejWienerUNI <ondrej.wiener@tul.cz>
2024-10-24 17:12:48 +0200

    #7 ADD pma07_custom_fragments_app (first commit)

---
+++ pma07_custom_fragments_app/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pma07_custom_fragments_app"
+include(":app")
+ 
